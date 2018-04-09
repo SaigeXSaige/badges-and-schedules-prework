79b7d755fa93ea89be40c8cc882bb313971b713a
Saige Leslie <saigeleslie@gmail.com>
2018-04-09 18:40:12 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ def assign_rooms(names)
   assignments = []
   
   while i < names.length
-      assignments.push("Hello, #{names[i]}! You'll be assigned to room #{i}!")
+      assignments.push("Hello, #{names[i]}! You'll be assigned to room #{i + 1}!")
     i += 1
   end
   return assignments

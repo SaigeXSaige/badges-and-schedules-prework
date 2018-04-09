547dea3168e7784861a0bd9f3707593519937fe5
Saige Leslie <saigeleslie@gmail.com>
2018-04-09 17:35:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,14 @@
 # Write your code here.
 def badge_maker(names)
-  puts 
+  puts "Hello, my name is #{names}."
 end
 
 def batch_badge_creator(names)
-  
+  badges = []
+  names.each |name| do
+    badges.push(badge_maker(name))
+  end
+  badges
 end
 
 def assign_rooms(names)
